91a24d24049b3efe36167b49bc2c3ce72e95e3ab
adrianportales <adrianportales135@gmail.com>
2023-06-08 21:52:17 -0400

    Respond to PING

--- src/Server.cs
+++ src/Server.cs
@@ -7,4 +7,9 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+
+// convert string to byte array
+byte[] data = System.Text.Encoding.ASCII.GetBytes("+PONG\r\n");
+
+socket.Send(data);
